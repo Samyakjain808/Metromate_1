0ea45ed71b7998347213f78105aa8bd58a34acf6
Samyakjain808 <jainsamyak5678@gmail.com>
2024-03-19 19:40:54 +0530

    first part project

---
+++ backend/models/HistoryModels.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const historySchema = new mongoose.Schema({
+    source:{
+        type:String, 
+        required:true,
+    },
+    // hi
+    destination:{
+        type:String,
+        required:true,
+    },
+    timeStamp:{
+        type:Date,
+        required:true,
+        default:Date.now,
+    }
+})
+
+module.exports = mongoose.model("History",historySchema);
